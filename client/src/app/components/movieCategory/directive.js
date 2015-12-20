decd47db1ac82e23e043fcb24b10b1f8dcc7e60e
Guillaume <grannaud@gmail.com>
2015-12-21 00:20:34 +0100

    aperçu 10 premier resultat pour page category (film/series)

--- client/src/app/components/movieCategory/directive.js
+++ client/src/app/components/movieCategory/directive.js
@@ -6,15 +6,21 @@
             restrict: 'EA',
             replace: true,
             templateUrl : './src/app/components/movieCategory/template.html',
-            scope: {},
+            scope: {
+                filter: "@",
+                name: "@",
+                id: "@"
+            },
             controllerAs: 'vm',
             bindToController: true,
 
             controller: function($log, MovieCategoryService, $stateParams) {
                 var vm = this;
-                var name = $stateParams.name;
-                MovieCategoryService.getMovie($stateParams.id).then(function(data){
-                    vm.categorymovies = data.data.results;
+                var filter = vm.filter;
+                var id = $stateParams.id ? $stateParams.id : vm.id;
+                var name = $stateParams.name ? $stateParams.name : vm.name;
+                MovieCategoryService.getMovie(id).then(function(data){
+                    vm.categorymovies = filter > 0 ? _.slice(data.data.results, 0, filter) : data.data.results;
                     vm.name = name;
                 });
             },
